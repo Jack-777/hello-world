29df89da174bdebf7302f02ce39b40ef01a8a2d6
Jack <jackjon@kth.se>
2019-10-30 00:17:17 +0100

    A python Hello world. This will be pushed to the python-file branch

---
+++ git_tut.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def main():
+	a = 1
+	b = 2
+	c = 3
+
+	print("Hello world! \n")
+	print("This is a Hello world that will be pushed to github! \n")
+	print(a,b,c)
+
+
+
+main()
